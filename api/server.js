649fbff369b0459e67ae0664be19f0b23cd2342a
Chantz-J <johnson.chantz@gmail.com>
2021-04-16 17:10:53 -0400

    routes all done and functioning

--- api/server.js
+++ api/server.js
@@ -2,16 +2,26 @@
 const express = require('express')
 const helmet = require('helmet')
 const server = express()
-server.use(helmet())
-server.use(express.json())
+
 
 //Routes
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
+
+server.use(helmet())
+server.use(express.json())
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+
 
 server.get('/', (req, res) => {
     res.status(200).json('Server (web sprint challenge) up and running! 😉 Head over to /api to get started!')
 })
 server.get('/api', (req, res) => {
-    res.status(200).json(`Welcome to the API!`)
+    res.status(200).json(`Welcome to the API! Try hitting these endpoints: 
+    /api/projects, /api/resources, /api/tasks `)
 })
-
+ 
 module.exports = server
